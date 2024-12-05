3854b47a8fb48b0203ba423c7c71e9b294848cc5
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2024-12-05 19:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+const url = document.getElementById("url");
+const buttonValue = document.getElementById("button")
+
+button.addEventListener("click", () =>{
+	const nameValue = document.getElementById("name").value;
+	const yearValue = document.getElementById("year").value;
+	if (nameValue && yearValue) {
+        // Create query string
+        const queryString = `?name=${nameValue}&year=${yearValue}`;
+        
+        // Update h3 element with new URL
+        document.getElementById("url").textContent = `https://localhost:8080/${queryString}`;
+      } 
+});
